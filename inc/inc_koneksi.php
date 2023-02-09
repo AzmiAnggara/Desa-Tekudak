29125778f0322ee7ef897b66f73597ffd06e0b3d
AzmiAnggara <azmianggara53@gmail.com>
2023-02-09 22:09:49 +0700

    first commit

---
+++ inc/inc_koneksi.php
@@ -0,0 +1,11 @@
+<?php 
+$host ="localhost";
+$user ="root";
+$pass ="";
+$db ="desapengadang";
+
+$koneksi =mysqli_connect($host,$user,$pass,$db);
+if(!$koneksi){
+    die("gagal terkoneksi");
+
+}
